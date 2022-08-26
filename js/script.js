6b5e40a1cb7876c92831d3027c3250646faabde2
Regor-Fran7 <100968732+Regor-Fran7@users.noreply.github.com>
2022-08-26 01:03:05 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ background_menu = document.getElementById("back_menu");
 
 function mostrar_menu(){
 
-    nav.style.right = "0px";
+    nav.style.left = "0px";
     background_menu.style.display = "block";
 }
 
 function ocultar_menu(){
 
-    nav.style.right = "-250px";
+    nav.style.left = "-250px";
     background_menu.style.display = "none"
 }
